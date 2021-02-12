ced4043d273e9cdeaebe65cb4602d958ca128f72
shubham-root <shubham.root@gmail.com>
2021-02-13 01:48:03 +0530

    updated for self config refresh

--- apis/auth.js
+++ apis/auth.js
@@ -5,17 +5,18 @@ module.exports = function (RED) {
     function GoogleSession(config) { 
         // the config information needs a runtime API to be set as creds
         const FastMQ = require('fastmq');
+        const nodeSchedule = require("node-schedule");
         var crypto = require('crypto');
         var localUserCache = {};
-        console.log(config);
         RED.nodes.createNode(this, config);
+
+        const refreshCreds = async () => {
+            
+        }
+
         this.name = config.name === "" || isNullOrUndefined(config.name)? "Google Authorization" : config.name;
-        // this.referenceId = this.credentials.referenceId;
-        // this.access_token = this.credentials.access_token;
-        // this.expiry_date = this.credentials.expiry_date;
         this.fastmqChannel = config.fastmqChannel === "" || isNullOrUndefined(config.fastmqChannel) ? "master" : config.fastmqChannel;
         this.fastmqTopic = config.fastmqTopic === "" || isNullOrUndefined(config.fastmqTopic) ? "refresh" : config.fastmqTopic;
-        console.log(this);
         const node = this;
         if (this.credentials.access_token && this.credentials.expiry_date) {
             this.oauth = {
@@ -29,27 +30,31 @@ module.exports = function (RED) {
             } else {
                 self.warn("Failed to authenticate with Google");
             }
-        }
+            if(node.credentials.expiry_date < Date.now()){
+                refreshCreds(node.fastmqChannel, node.fastmqTopic, node.credentials.referenceId);
+            }
 
-        node.refreshCreds = async () => {
-            var requestChannel;
-            // create a client with 'requestChannel' channel name and connect to server.
-            FastMQ.Client.connect('requestChannel', this.fastmqChannel).then((channel) => { // client connected
-                requestChannel = channel;
-                // send request to 'master' channel  with topic 'test_cmd' and JSON format payload.
-                let reqPayload = {
-                    data: {
-                        referenceId: this.referenceId
-                    }
-                };
-                return requestChannel.request('master', this.fastmqTopic, reqPayload, 'json');
-            }).then((result) => {
-                console.log('Got response from master, data:' + result.payload.data);
-                // client channel disconnect
-                requestChannel.disconnect();
-            }).catch((err) => {
-                console.log('Got error:', err.stack);
-            });
+            function refreshCreds (fastmqChannel, fastmqTopic, referenceId) {
+                var requestChannel;
+                // create a client with 'requestChannel' channel name and connect to server.
+                FastMQ.Client.connect('requestChannel', fastmqChannel).then((channel) => { // client connected
+                    requestChannel = channel;
+                    // send request to 'master' channel  with topic 'test_cmd' and JSON format payload.
+                    let reqPayload = {
+                        data: {
+                            referenceId: referenceId
+                        }
+                    };
+                    return requestChannel.request(fastmqChannel, fastmqTopic, reqPayload, 'json');
+                }).then((result) => {
+                    console.log('Got response from master, data:' + result.payload.data);
+                    // client channel disconnect
+                    requestChannel.disconnect();
+                }).catch((err) => {
+                    console.log('Got error:', err.stack);
+                });
+            }
+            nodeSchedule.scheduleJob(new Date(node.credentials.expiry_date - 5000), function () {refreshCreds(node.fastmqChannel, node.fastmqTopic, node.credentials.referenceId)})
         }
     }
 
